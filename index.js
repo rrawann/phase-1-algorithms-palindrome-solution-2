adf4473446851130d7355be607d110f8a2b86f06
Rawan <rawnq.sb@gmail.com>
2023-03-19 23:39:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //loop through string halfway
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    //compare firstletter with last letter
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
@@ -11,6 +20,7 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
